1ef795f5c607daffa33bc4910f52666e2e2daa08
datodato788 <151830320+datodato788@users.noreply.github.com>
2024-03-23 15:09:18 +0400

    davamate footeri da navbari

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from '../footer/Footer.module.css'
+
+function Footer() {
+  return (
+    <div className={styles.background}>
+    <div className={styles.a}>@All Rights Reserved</div>
+    </div>
+  )
+}
+
+export default Footer
